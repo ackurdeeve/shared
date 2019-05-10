2e17eecbb52c67851de9ae427b6c7c65bf965b9b
Ackurdeeve <15801959+ackurdeeve@users.noreply.github.com>
2019-05-10 12:26:52 +0800

    Add files via upload

---
+++ probcal.js
@@ -0,0 +1,397 @@
+var _0x290b = ['chess_sniper', 'chess_sf', 'chess_viper', 'chess_lyc', 'chess_pa', 'chess_veno', 'chess_lina', 'chess_tb', 'chess_kunkka', 'chess_doom', 'chess_troll', 'chess_ta', 'chess_dk', 'chess_light', 'chess_th', 'chess_tech', 'chess_dp', 'chess_zeus', 'chess_nec_ssr', 'chess_ck_ssr', 'function\x20*\x5c(\x20*\x5c)', '\x5c+\x5c+\x20*(?:_0x(?:[a-f0-9]){4,6}|(?:\x5cb|\x5cd)[a-z0-9]{1,4}(?:\x5cb|\x5cd))', 'chain', 'test', 'input', 'INIT\x20CHESS\x20POOL\x20OK!', 'indexOf', 'fur', 'substring', 'length', 'push', 'log', '个棋子（', 'getElementById', 'field1', 'value', 'new_chess_list_str', 'new_chess_list_table', 'chess_io', '棋库空了', 'undefined', 'floor', 'random', 'string', 'constructor', 'while\x20(true)\x20{}', 'apply', 'counter', 'debu', 'gger', 'call', 'action', '斧王1', '魅惑魔女1', '食人魔魔法师1', '巨牙海民1', '卓尔游侠1', '赏金猎人1', '发条技师1', '暗影萨满1', '修补匠1', '敌法师1', '天界战神1', '水晶室女2', '兽王2', '剑圣2', '痛苦女王2', '精灵龙2', '巫医2', '鱼人守卫2', '混沌骑士2', '月之骑士2', '先知2', '变体精灵2', '斯拉克2', '米拉娜2', '狼人3', '剧毒术士3', '全能骑士3', '闪电幽魂3', '风行者3', '幻影刺客3', '狙击手3', '冥界亚龙3', '影魔3', '秀逗魔导师3', '虚空假面3', '树精卫士3', '末日使者4', '巨魔战将4', '光之守卫4', '圣堂刺客4', '干扰者4', '蛇发女妖4', '龙骑士4', '利爪德鲁伊4', '矮人直升机5', '巫妖5', '潮汐猎人5', '谜团5', '地精工程师5', '死亡先知5', '众神之王5', '卡尔5', '斯温5', '精灵守卫5', '谜团SSR', 'chess_tusk', 'chess_axe', 'chess_eh', 'chess_om', 'chess_clock', 'chess_ss', 'chess_bh', 'chess_bat', 'chess_dr', 'chess_tk', 'chess_am', 'chess_tiny', 'chess_mars', 'chess_bm', 'chess_jugg', 'chess_puck', 'chess_ck', 'chess_luna', 'chess_wd', 'chess_cm', 'chess_morph', 'chess_pom', 'chess_razor'];
+(function (_0x2e776d, _0x2bfffd) {
+  var _0x31d8fb = function (_0xe126d7) {
+    while (--_0xe126d7) {
+      _0x2e776d['push'](_0x2e776d['shift']());
+    }
+  };
+  _0x31d8fb(++_0x2bfffd);
+}(_0x290b, 0x1ba));
+var _0x28bf = function (_0x10f3f2, _0x3002af) {
+  _0x10f3f2 = _0x10f3f2 - 0x0;
+  var _0x15848a = _0x290b[_0x10f3f2];
+  return _0x15848a;
+};
+CHESS_POOL_SIZE = 0x5;
+chess_pool_times = 0x6;
+chess_list_trans = {
+  'chess_axe': _0x28bf('0x0'),
+  'chess_eh': _0x28bf('0x1'),
+  'chess_om': _0x28bf('0x2'),
+  'chess_tusk': _0x28bf('0x3'),
+  'chess_dr': _0x28bf('0x4'),
+  'chess_bh': _0x28bf('0x5'),
+  'chess_clock': _0x28bf('0x6'),
+  'chess_ss': _0x28bf('0x7'),
+  'chess_bat': '蝙蝠骑士1',
+  'chess_tk': _0x28bf('0x8'),
+  'chess_am': _0x28bf('0x9'),
+  'chess_tiny': '小小1',
+  'chess_mars': _0x28bf('0xa'),
+  'chess_cm': _0x28bf('0xb'),
+  'chess_bm': _0x28bf('0xc'),
+  'chess_jugg': _0x28bf('0xd'),
+  'chess_shredder': '伐木机2',
+  'chess_qop': _0x28bf('0xe'),
+  'chess_puck': _0x28bf('0xf'),
+  'chess_wd': _0x28bf('0x10'),
+  'chess_slardar': _0x28bf('0x11'),
+  'chess_ck': _0x28bf('0x12'),
+  'chess_luna': _0x28bf('0x13'),
+  'chess_fur': _0x28bf('0x14'),
+  'chess_morph': _0x28bf('0x15'),
+  'chess_slark': _0x28bf('0x16'),
+  'chess_pom': _0x28bf('0x17'),
+  'chess_lyc': _0x28bf('0x18'),
+  'chess_veno': _0x28bf('0x19'),
+  'chess_ok': _0x28bf('0x1a'),
+  'chess_razor': _0x28bf('0x1b'),
+  'chess_wr': _0x28bf('0x1c'),
+  'chess_sk': '沙王3',
+  'chess_pa': _0x28bf('0x1d'),
+  'chess_abaddon': '死亡骑士3',
+  'chess_sniper': _0x28bf('0x1e'),
+  'chess_viper': _0x28bf('0x1f'),
+  'chess_sf': _0x28bf('0x20'),
+  'chess_lina': _0x28bf('0x21'),
+  'chess_tb': '灵魂守卫3',
+  'chess_riki': '隐形刺客3',
+  'chess_fv': _0x28bf('0x22'),
+  'chess_tp': _0x28bf('0x23'),
+  'chess_doom': _0x28bf('0x24'),
+  'chess_kunkka': '海军上将4',
+  'chess_troll': _0x28bf('0x25'),
+  'chess_light': _0x28bf('0x26'),
+  'chess_nec': '死灵法师4',
+  'chess_ta': _0x28bf('0x27'),
+  'chess_ga': '炼金术士4',
+  'chess_disruptor': _0x28bf('0x28'),
+  'chess_medusa': _0x28bf('0x29'),
+  'chess_dk': _0x28bf('0x2a'),
+  'chess_ld': _0x28bf('0x2b'),
+  'chess_gyro': _0x28bf('0x2c'),
+  'chess_lich': _0x28bf('0x2d'),
+  'chess_th': _0x28bf('0x2e'),
+  'chess_enigma': _0x28bf('0x2f'),
+  'chess_tech': _0x28bf('0x30'),
+  'chess_dp': _0x28bf('0x31'),
+  'chess_zeus': _0x28bf('0x32'),
+  'chess_kael': _0x28bf('0x33'),
+  'chess_sven': _0x28bf('0x34'),
+  'chess_io': _0x28bf('0x35'),
+  'chess_nec_ssr': '死灵法师SSR',
+  'chess_ck_ssr': '混沌骑士SSR',
+  'chess_enigma_ssr': _0x28bf('0x36')
+};
+chess_list_by_mana = {
+  1: [_0x28bf('0x37'), _0x28bf('0x38'), _0x28bf('0x39'), _0x28bf('0x3a'), _0x28bf('0x3b'), _0x28bf('0x3c'), _0x28bf('0x3d'), _0x28bf('0x3e'), _0x28bf('0x3f'), _0x28bf('0x40'), _0x28bf('0x41'), _0x28bf('0x42'), _0x28bf('0x43')],
+  2: [_0x28bf('0x44'), _0x28bf('0x45'), 'chess_shredder', _0x28bf('0x46'), _0x28bf('0x47'), 'chess_slardar', _0x28bf('0x48'), 'chess_qop', _0x28bf('0x49'), _0x28bf('0x4a'), 'chess_fur', _0x28bf('0x4b'), _0x28bf('0x4c')],
+  3: ['chess_ok', _0x28bf('0x4d'), 'chess_wr', 'chess_abaddon', _0x28bf('0x4e'), _0x28bf('0x4f'), _0x28bf('0x50'), _0x28bf('0x51'), _0x28bf('0x52'), _0x28bf('0x53'), _0x28bf('0x54'), _0x28bf('0x55'), 'chess_tp'],
+  4: [_0x28bf('0x56'), _0x28bf('0x57'), _0x28bf('0x58'), 'chess_nec', _0x28bf('0x59'), 'chess_medusa', 'chess_disruptor', 'chess_ga', _0x28bf('0x5a'), _0x28bf('0x5b'), 'chess_ld'],
+  5: ['chess_gyro', _0x28bf('0x5c'), 'chess_enigma', _0x28bf('0x5d'), _0x28bf('0x5e'), _0x28bf('0x5f')]
+};
+chess_gailv = {
+  1: {101: 0x2},
+  2: {70: 0x2},
+  3: {60: 0x2, 95: 0x3},
+  4: {50: 0x2, 85: 0x3},
+  5: {40: 0x2, 75: 0x3, 98: 0x4},
+  6: {33: 0x2, 63: 0x3, 93: 0x4},
+  7: {30: 0x2, 60: 0x3, 90: 0x4},
+  8: {24: 0x2, 54: 0x3, 84: 0x4, 99: 0x5},
+  9: {22: 0x2, 52: 0x3, 77: 0x4, 97: 0x5},
+  10: {19: 0x2, 44: 0x3, 69: 0x4, 94: 0x5}
+};
+CHESS_INIT_COUNT = {1: 0x9, 2: 0x6, 3: 0x5, 4: 0x3, 5: 0x2};
+chess_list_ssr = [_0x28bf('0x60'), _0x28bf('0x61')];
+chess_2_mana = {
+  'chess_tusk': 0x1,
+  'chess_axe': 0x1,
+  'chess_eh': 0x1,
+  'chess_om': 0x1,
+  'chess_clock': 0x1,
+  'chess_ss': 0x1,
+  'chess_bh': 0x1,
+  'chess_bat': 0x1,
+  'chess_dr': 0x1,
+  'chess_tk': 0x1,
+  'chess_bm': 0x2,
+  'chess_jugg': 0x2,
+  'chess_shredder': 0x2,
+  'chess_puck': 0x2,
+  'chess_ck': 0x2,
+  'chess_slardar': 0x2,
+  'chess_luna': 0x2,
+  'chess_tp': 0x3,
+  'chess_qop': 0x2,
+  'chess_am': 0x1,
+  'chess_wd': 0x2,
+  'chess_cm': 0x2,
+  'chess_light': 0x4,
+  'chess_ok': 0x3,
+  'chess_razor': 0x3,
+  'chess_wr': 0x3,
+  'chess_sk': 0x3,
+  'chess_abaddon': 0x3,
+  'chess_slark': 0x2,
+  'chess_sniper': 0x3,
+  'chess_sf': 0x3,
+  'chess_viper': 0x3,
+  'chess_lyc': 0x3,
+  'chess_pa': 0x3,
+  'chess_kunkka': 0x4,
+  'chess_doom': 0x4,
+  'chess_lina': 0x3,
+  'chess_troll': 0x4,
+  'chess_veno': 0x3,
+  'chess_nec': 0x4,
+  'chess_ta': 0x4,
+  'chess_medusa': 0x4,
+  'chess_disruptor': 0x4,
+  'chess_ga': 0x4,
+  'chess_dk': 0x4,
+  'chess_gyro': 0x5,
+  'chess_lich': 0x5,
+  'chess_th': 0x5,
+  'chess_enigma': 0x5,
+  'chess_tech': 0x5,
+  'chess_fur': 0x2,
+  'chess_ld': 0x4,
+  'chess_tiny': 0x1,
+  'chess_tb': 0x3,
+  'chess_morph': 0x2,
+  'chess_nec_ssr': 0xa,
+  'chess_ck_ssr': 0xf,
+  'chess_enigma_ssr': 0xf,
+  'chess_kael': 0x5,
+  'chess_sven': 0x5,
+  'chess_riki': 0x3,
+  'chess_pom': 0x2,
+  'chess_dp': 0x5,
+  'chess_fv': 0x3,
+  'chess_zeus': 0x5,
+  'chess_mars': 0x1,
+  'chess_io': 0x5
+};
+var chess_pool = {1: [], 2: [], 3: [], 4: [], 5: []};
+
+function InitChessPool() {
+  var _0x269943 = function () {
+    var _0x1f86cf = !![];
+    return function (_0x9bafd, _0x2c6581) {
+      var _0x6055d3 = _0x1f86cf ? function () {
+        if (_0x2c6581) {
+          var _0x7e3346 = _0x2c6581['apply'](_0x9bafd, arguments);
+          _0x2c6581 = null;
+          return _0x7e3346;
+        }
+      } : function () {
+      };
+      _0x1f86cf = ![];
+      return _0x6055d3;
+    };
+  }();
+  (function () {
+    _0x269943(this, function () {
+      var _0x576ec4 = new RegExp(_0x28bf('0x62'));
+      var _0x39433a = new RegExp(_0x28bf('0x63'), 'i');
+      var _0x563f68 = _0x62f670('init');
+      if (!_0x576ec4['test'](_0x563f68 + _0x28bf('0x64')) || !_0x39433a[_0x28bf('0x65')](_0x563f68 + _0x28bf('0x66'))) {
+        _0x563f68('0');
+      } else {
+        _0x62f670();
+      }
+    })();
+  }());
+  if (chess_pool_times !== null) {
+    chess_pool_times = 0x5;
+  } else {
+    chess_pool_times = 0x6;
+  }
+  for (var _0x4d37d0 in chess_list_by_mana) {
+    for (var _0x45ab8f in chess_list_by_mana[_0x4d37d0]) {
+      chess_count = CHESS_INIT_COUNT[_0x4d37d0] * chess_pool_times;
+      chess = chess_list_by_mana[_0x4d37d0][_0x45ab8f];
+      for (let _0x5bc56f = 0x0; _0x5bc56f < chess_count; _0x5bc56f++) {
+        AddAChessToChessPool(chess);
+      }
+    }
+  }
+  console['log'](_0x28bf('0x67'));
+  return chess_pool;
+}
+
+function AddAChessToChessPool(_0x591596) {
+  var _0x15774e = 0x1;
+  if (_0x591596[_0x28bf('0x68')]('ssr') > -0x1) {
+    return;
+  }
+  if (_0x591596[_0x28bf('0x68')]('11') > -0x1) {
+    if (_0x591596[_0x28bf('0x68')]('tp') > -0x1 || _0x591596[_0x28bf('0x68')]('eh') > -0x1 || _0x591596[_0x28bf('0x68')]('ld') > -0x1 || _0x591596[_0x28bf('0x68')](_0x28bf('0x69')) > -0x1) {
+      _0x591596 = _0x591596[_0x28bf('0x6a')](0x0, _0x591596[_0x28bf('0x6b')] - 0x2);
+      _0x15774e = 0x4;
+    } else {
+      _0x591596 = _0x591596['substring'](0x0, _0x591596[_0x28bf('0x6b')] - 0x2);
+      _0x15774e = 0x9;
+    }
+  }
+  if (_0x591596['indexOf']('1') > -0x1) {
+    if (_0x591596[_0x28bf('0x68')]('tp') > -0x1 || _0x591596[_0x28bf('0x68')]('eh') > -0x1 || _0x591596[_0x28bf('0x68')]('ld') > -0x1 || _0x591596[_0x28bf('0x68')](_0x28bf('0x69')) > -0x1) {
+      _0x591596 = _0x591596[_0x28bf('0x6a')](0x0, _0x591596[_0x28bf('0x6b')] - 0x1);
+      _0x15774e = 0x2;
+    } else {
+      _0x591596 = _0x591596['substring'](0x0, _0x591596[_0x28bf('0x6b')] - 0x1);
+      _0x15774e = 0x3;
+    }
+  }
+  for (let _0x382c3e = 0x0; _0x382c3e < _0x15774e; _0x382c3e++) {
+    if (chess_2_mana[_0x591596] !== '') {
+      cost = chess_2_mana[_0x591596];
+    }
+    chess_pool[cost][_0x28bf('0x6c')](_0x591596);
+  }
+  return {'chess': _0x591596, 'maxcount': _0x15774e};
+}
+
+function PrintChessPool() {
+  count = [];
+  count_all = 0x0;
+  for (let _0x388491 = 0x1; _0x388491 < 0x6; _0x388491++) {
+    count[_0x388491] = chess_pool[_0x388491][_0x28bf('0x6b')];
+    count_all = count_all + count[_0x388491];
+  }
+  console[_0x28bf('0x6d')]('棋库有', count_all, _0x28bf('0x6e'), count[0x1], '/', count[0x2], '/', count[0x3], '/', count[0x4], '/', count[0x5], '）');
+}
+
+function RoundShowHtml() {
+  var _0x175152 = 0x1;
+  level = document[_0x28bf('0x6f')](_0x28bf('0x70'))[_0x28bf('0x71')];
+  if (level < 0x1 || level > 0xa) {
+    alert('请输入正确的等级');
+    return;
+  }
+  return Draw5ChessAndShow(_0x175152);
+}
+
+function PrepareARound(_0x215ccc, _0x59044) {
+  level = _0x59044;
+  return Draw5ChessAndShow(_0x215ccc);
+}
+
+function Draw5ChessAndShow(_0x3424ea) {
+  var _0x501b43 = RandomNDrawChessNew(_0x3424ea, 0x5);
+  var _0x24a8c3 = _0x501b43[_0x28bf('0x72')];
+  var _0xe6e39e = _0x501b43[_0x28bf('0x73')];
+  console[_0x28bf('0x6d')]('=========汇总输出============');
+  for (var _0x5ee74c = 0x0; _0x5ee74c < _0xe6e39e[_0x28bf('0x6b')]; _0x5ee74c++) {
+    console[_0x28bf('0x6d')](chess_list_trans[_0xe6e39e[_0x5ee74c]]);
+  }
+  document[_0x28bf('0x6f')]('field2')['value'] = _0x24a8c3;
+  return _0xe6e39e;
+}
+
+function RandomNDrawChessNew(_0x2fc262, _0x4d6467) {
+  var _0x30fdcd = '';
+  var _0x3f1bc5 = [];
+  var _0x705ca4 = 0x0;
+  while (_0x705ca4 < _0x4d6467) {
+    var _0x3c2be3 = RandomDrawChessNew(_0x2fc262);
+    if (_0x3c2be3 !== '') {
+      _0x30fdcd = _0x30fdcd + chess_list_trans[_0x3c2be3] + ',';
+      _0x3f1bc5[_0x28bf('0x6c')](_0x3c2be3);
+      _0x705ca4 = _0x705ca4 + 0x1;
+    }
+  }
+  return {'new_chess_list_str': _0x30fdcd, 'new_chess_list_table': _0x3f1bc5};
+}
+
+function RandomDrawChessNew(_0x51f4df) {
+  var _0x32cdcc = RandomInt(0x1, 0x64);
+  var _0x254439 = 0x1;
+  var _0x20e0b9 = 0x0;
+  var _0x3dc2f7 = level;
+  var _0x817e59 = [];
+  var _0x3fdefb = RandomInt(0x1, 0x2710);
+  var _0x900910 = RandomInt(0x1, 0x2710);
+  if (level >= 0x7 && _0x3fdefb <= 0x1 && _0x900910 <= 0x1) {
+    var _0x2f4e51 = chess_list_ssr[_0x28bf('0x6b')];
+    var _0x1f804b = chess_list_ssr[RandomInt(0x1, _0x2f4e51)];
+  } else if (_0x3fdefb <= 0x1e) {
+    _0x1f804b = _0x28bf('0x74');
+  } else {
+    if (chess_gailv[_0x3dc2f7] !== '') {
+      for (var _0x349059 in chess_gailv[_0x3dc2f7]) {
+        var _0x3e3ce5 = chess_gailv[_0x3dc2f7][_0x349059];
+        if (_0x32cdcc > _0x349059 && _0x20e0b9 <= _0x349059) {
+          _0x20e0b9 = _0x349059;
+          _0x254439 = _0x3e3ce5;
+        }
+      }
+    }
+    _0x1f804b = DrawAChessFromChessPool(_0x254439, _0x817e59);
+  }
+  return _0x1f804b;
+}
+
+function DrawAChessFromChessPool(_0x3ca4a5, _0x21e5d2) {
+  var _0x529e38 = chess_pool[_0x3ca4a5][_0x28bf('0x6b')];
+  if (_0x21e5d2 === '') {
+    _0x21e5d2 = [];
+  }
+  if (chess_pool[_0x3ca4a5] === '' || _0x529e38 < 0x1) {
+    console[_0x28bf('0x6d')](_0x28bf('0x75'));
+    return '';
+  }
+  var _0x3bf3fb = RandomInt(0x1, _0x529e38);
+  var _0x4f6ac0 = chess_pool[_0x3ca4a5][_0x3bf3fb - 0x1];
+  if (typeof _0x4f6ac0 == _0x28bf('0x76')) {
+    console[_0x28bf('0x6d')](error);
+  }
+  '';
+  return _0x4f6ac0;
+}
+
+function RandomInt(_0x3dca2c, _0x224fce) {
+  var _0xe290c7 = Math[_0x28bf('0x77')](Math[_0x28bf('0x78')]() * (_0x224fce - _0x3dca2c + 0x1)) + _0x3dca2c;
+  return _0xe290c7;
+}
+
+function check_trans() {
+  for (var _0x21f462 = 0x1; _0x21f462 < 0x6; _0x21f462++) {
+    for (var _0x390cd0 = 0x0; _0x390cd0 < chess_pool[_0x21f462][_0x28bf('0x6b')]; _0x390cd0++) {
+      console[_0x28bf('0x6d')](chess_list_trans[chess_pool[_0x21f462][_0x390cd0]]);
+    }
+  }
+}
+
+InitChessPool();
+PrintChessPool();
+
+function _0x62f670(_0x383d53) {
+  function _0x35bb91(_0x400c37) {
+    if (typeof _0x400c37 === _0x28bf('0x79')) {
+      return function (_0x23439d) {
+      }[_0x28bf('0x7a')](_0x28bf('0x7b'))[_0x28bf('0x7c')](_0x28bf('0x7d'));
+    } else {
+      if (('' + _0x400c37 / _0x400c37)[_0x28bf('0x6b')] !== 0x1 || _0x400c37 % 0x14 === 0x0) {
+        (function () {
+          return !![];
+        }[_0x28bf('0x7a')](_0x28bf('0x7e') + _0x28bf('0x7f'))[_0x28bf('0x80')](_0x28bf('0x81')));
+      } else {
+        (function () {
+          return ![];
+        }[_0x28bf('0x7a')](_0x28bf('0x7e') + 'gger')[_0x28bf('0x7c')]('stateObject'));
+      }
+    }
+    _0x35bb91(++_0x400c37);
+  }
+
+  try {
+    if (_0x383d53) {
+      return _0x35bb91;
+    } else {
+      _0x35bb91(0x0);
+    }
+  } catch (_0x471d19) {
+  }
+}
